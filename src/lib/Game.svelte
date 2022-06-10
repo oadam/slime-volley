78470712747e2c7e3d9bc225bdf7bac1034ee3a6
Olivier Adam <olivier@getunlatch.com>
2022-06-10 15:09:59 +0200

    2 players displayed

---
+++ src/lib/Game.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Player from './Player.svelte';
+  let player1 = {
+	eyeAngle: 20,
+x:100,
+y:0
+	};
+let player2: typeof player1 = {
+	eyeAngle: 160,
+x:400,
+y:0
+}
+</script>
+
+<Player {...player1} />
+<Player {...player2} />
+
+
+<svg width="10" height="50" fill="black">
+	<rect width="100%" height="100%"></rect>
+</svg>
+<svg width="30" height="30" fill="yellow">
+	<circle cx="15" cy="15" r="15"></circle>
+</svg>
+
---
+++ src/lib/Player.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let eyeAngle = 40;
+	export let x = 0;
+	export let y = 0;
+</script>
+
+<svg width="200" height="100" style:transform="translate({x}px, {y}px)">
+	<circle cx="100" cy="100" r="100" fill="red"></circle>
+	<g transform="translate(100 100) rotate(-{eyeAngle})">
+		<circle cx="80" cy="0" r="10" fill="white"></circle>
+		<circle cx="80" cy="0" r="5" fill="black"></circle>
+	</g>
+</svg>
