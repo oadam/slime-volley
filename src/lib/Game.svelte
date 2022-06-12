4d195a1b53ba16e6c801b75ef33be9ce8193e3b4
Olivier Adam <olivier@getunlatch.com>
2022-06-13 01:05:48 +0200

    2 players + box2d

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -1,25 +1,205 @@
 <script lang="ts">
-	import Player from './Player.svelte';
-  let player1 = {
-	eyeAngle: 20,
-x:100,
-y:0
-	};
-let player2: typeof player1 = {
-	eyeAngle: 160,
-x:400,
-y:0
-}
+  import Player from "./Player.svelte";
+  import { onMount } from "svelte";
+  import {
+    b2Vec2,
+    b2World,
+    b2PolygonShape,
+    b2CircleShape,
+    b2ChainShape,
+    b2FixtureDef,
+    b2BodyDef,
+    b2ContactListener,
+    b2BodyType,
+    XY,
+  } from "@flyover/box2d";
+
+  const SCREEN_WIDTH = 800;
+  const SCREEN_HEIGHT = 600;
+  const WIDTH = 10;
+  const PLAYER_RADIUS = 1;
+  const PLAYER_CHAIN_SIZE = 100;
+  const JUMP_HEIGHT = 3;
+  const BALL_RADIUS = 0.2;
+  const BALL_RESTITUTION = 0.8;
+  const NET_HEIGHT = 2.4;
+  const GROUND_THICKNESS = 0.1;
+  const NET_THICKNESS = 0.05;
+  const PLAYER_STARTING_POS = 10 * 0.7;
+  const BALL_STARTING_POS = {
+    x: 0.1 * WIDTH,
+    y: 3 * NET_HEIGHT,
+  };
+  const GRAVITY = 10;
+  const PHYSICS = {
+    timeStep: 1 / 60,
+    velocityIterations: 6,
+    positionIterations: 2,
+  };
+
+  const gravity = new b2Vec2(0, -GRAVITY);
+  const world = new b2World(gravity);
+  const groundBodyDef = new b2BodyDef();
+  groundBodyDef.position.Set(0, -GROUND_THICKNESS / 2);
+  const groundBody = world.CreateBody(groundBodyDef);
+  // Define the ground box shape.
+  const groundBox = new b2PolygonShape();
+  // The extents are the half-widths of the box.
+  groundBox.SetAsBox(WIDTH, GROUND_THICKNESS / 2);
+  // Add the ground fixture to the ground body.
+  groundBody.CreateFixture(groundBox, 0);
+
+  const ballDef = new b2BodyDef();
+  ballDef.type = b2BodyType.b2_dynamicBody;
+  ballDef.position.Set(BALL_STARTING_POS.x, BALL_STARTING_POS.y);
+  const ballBody = world.CreateBody(ballDef);
+  const ballShape = new b2CircleShape();
+  ballShape.Set({ x: 0, y: 0 }, BALL_RADIUS);
+  const ballFixtureDef = new b2FixtureDef();
+  ballFixtureDef.shape = ballShape;
+  ballFixtureDef.density = 1;
+  ballFixtureDef.restitution = BALL_RESTITUTION;
+  ballFixtureDef.friction = BALL_RESTITUTION;
+  ballBody.CreateFixture(ballFixtureDef);
+  let ballPosition = ballBody.GetPosition();
+
+  function makePlayer(startX: number) {
+    const playerDef = new b2BodyDef();
+    playerDef.type = b2BodyType.b2_kinematicBody;
+    playerDef.position.Set(startX, 0);
+    const playerBody = world.CreateBody(playerDef);
+    const playerShape = new b2ChainShape();
+    const playerPoints = [];
+    for (let p = 0; p < PLAYER_CHAIN_SIZE; p++) {
+      const angle = Math.PI * (1 - (p / (PLAYER_CHAIN_SIZE - 1)));
+      playerPoints.push({
+        x: Math.cos(angle) * PLAYER_RADIUS,
+        y: Math.sin(angle) * PLAYER_RADIUS,
+      });
+    }
+    playerShape.CreateLoop(playerPoints);
+    const playerFixtureDef = new b2FixtureDef();
+    playerFixtureDef.shape = playerShape;
+    playerFixtureDef.density = 1;
+    playerFixtureDef.restitution = BALL_RESTITUTION;
+    playerBody.CreateFixture(playerFixtureDef);
+    return playerBody;
+  }
+
+  let player1Body = makePlayer(BALL_STARTING_POS.x*1.02)
+  let player1Position = player1Body.GetPosition();
+  let player2Body = makePlayer(-WIDTH / 2)
+  let player2Position = player2Body.GetPosition();
+
+  class MyListener extends b2ContactListener {
+    override BeginContact(contact): void {
+    }
+  }
+  world.SetContactListener(new MyListener());
+  /*function maybeBounceBall(
+    wallAngle: number,
+    wallDistance: number,
+    wallSpeed: number,
+    restitution: number
+  ): boolean {
+    const dDistance =
+      PHYSICS_LOOP_MS *
+      (Math.sin(wallAngle) * ball.vx - Math.cos(wallAngle) * ball.vy);
+    if (wallDistance + dDistance > 0) {
+      return false;
+    }
+    const timeBeforeBounce = (-wallDistance / dDistance) * PHYSICS_LOOP_MS;
+    console.assert(timeBeforeBounce > 0);
+    console.assert(timeBeforeBounce < PHYSICS_LOOP_MS);
+    ball.x += timeBeforeBounce * ball.vx;
+    ball.y += timeBeforeBounce * ball.vy;
+    const oldVAngle = Math.atan(ball.vy / ball.vx);
+    const newVAngle = Math.PI - oldVAngle + 2 * wallAngle;
+    const timeAfterBounce = PHYSICS_LOOP_MS - timeBeforeBounce;
+    const oldV = Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
+    const newV = restitution * oldV;
+    ball.vx = newV * Math.cos(newVAngle);
+    ball.vy = -newV * Math.sin(newVAngle);
+    ball.x += timeAfterBounce * ball.vx;
+    ball.y += timeAfterBounce * ball.vy;
+    return true;
+  }*/
+
+  let lastPhysics = performance.now() / 1000;
+  function runPhysics() {
+    const steps = Math.floor(
+      (performance.now() / 1000 - lastPhysics) / PHYSICS.timeStep
+    );
+    lastPhysics += steps * PHYSICS.timeStep;
+    for (let i = 0; i < steps; i++) {
+      world.Step(
+        PHYSICS.timeStep,
+        PHYSICS.velocityIterations,
+        PHYSICS.positionIterations
+      );
+      // useless assignments to force redraw
+      ballPosition = ballBody.GetPosition();
+      player1Position = player1Body.GetPosition();
+      player2Position = player2Body.GetPosition();
+    }
+  }
+
+  onMount(async () => {
+    let i = 0;
+    let rid = requestAnimationFrame(function update() {
+      i++;
+      runPhysics();
+      rid = requestAnimationFrame(update);
+    });
+    return () => cancelAnimationFrame(rid);
+  });
+
+  const computePlayerAngle = (player: XY) => {
+    const atan =
+      (180 / Math.PI) *
+      Math.atan(-(player.y - ballPosition.y) / (player.x - ballPosition.x));
+    const inBack = ballPosition.x < player.x ? 180 : 0;
+    return 360 + inBack - atan;
+  };
 </script>
 
-<Player {...player1} />
-<Player {...player2} />
-
-
-<svg width="10" height="50" fill="black">
-	<rect width="100%" height="100%"></rect>
+<svg
+  class="game"
+  style:width="{SCREEN_WIDTH}px"
+  style:height="{SCREEN_HEIGHT}px"
+>
+  <g
+    transform="
+      translate({SCREEN_WIDTH / 2} {SCREEN_HEIGHT})
+      scale({SCREEN_WIDTH / WIDTH} {-SCREEN_WIDTH / WIDTH})
+    "
+  >
+    <Player
+      color="red"
+      radius={PLAYER_RADIUS}
+      eyeAngle={computePlayerAngle(player1Position)}
+      x={player1Position.x}
+      y={player1Position.y}
+    />
+    <Player
+      color="blue"
+      radius={PLAYER_RADIUS}
+      eyeAngle={computePlayerAngle(player2Position)}
+      x={player2Position.x}
+      y={player2Position.y}
+    />
+    <circle
+      fill="yellow"
+      cx={ballPosition.x}
+      cy={ballPosition.y}
+      r={BALL_RADIUS}
+    />
+  </g>
 </svg>
-<svg width="30" height="30" fill="yellow">
-	<circle cx="15" cy="15" r="15"></circle>
-</svg>
 
+<style>
+  .game {
+    position: relative;
+    background: lightblue;
+  }
+</style>
--- src/lib/Player.svelte
+++ src/lib/Player.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	export let color = 'red';
+	export let radius = 1;
 	export let eyeAngle = 40;
 	export let x = 0;
 	export let y = 0;
 </script>
 
-<svg width="200" height="100" style:transform="translate({x}px, {y}px)">
-	<circle cx="100" cy="100" r="100" fill="red"></circle>
-	<g transform="translate(100 100) rotate(-{eyeAngle})">
-		<circle cx="80" cy="0" r="10" fill="white"></circle>
-		<circle cx="80" cy="0" r="5" fill="black"></circle>
+<g transform="translate({x} {y})">
+	<circle cx="0" cy="0" r={radius} fill={color}></circle>
+	<g transform="rotate({eyeAngle})">
+		<circle cx={0.8*radius} cy="0" r={0.1*radius} fill="white"></circle>
+		<circle cx={0.8*radius} cy="0" r={0.05*radius} fill="black"></circle>
 	</g>
-</svg>
+</g>
