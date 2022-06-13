f237a6aca7fd726997338870f33fae4c896303c9
Olivier Adam <olivier@getunlatch.com>
2022-06-13 09:39:26 +0200

    players + net + contact listener

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -6,29 +6,31 @@
     b2World,
     b2PolygonShape,
     b2CircleShape,
+    b2Contact,
     b2ChainShape,
     b2FixtureDef,
     b2BodyDef,
     b2ContactListener,
     b2BodyType,
     XY,
+    b2Body,
   } from "@flyover/box2d";
 
   const SCREEN_WIDTH = 800;
   const SCREEN_HEIGHT = 600;
   const WIDTH = 10;
   const PLAYER_RADIUS = 1;
-  const PLAYER_CHAIN_SIZE = 100;
+  const PLAYER_CHAIN_SIZE = 20;
   const JUMP_HEIGHT = 3;
   const BALL_RADIUS = 0.2;
   const BALL_RESTITUTION = 0.8;
-  const NET_HEIGHT = 2.4;
+  const NET_HEIGHT = 1.4;
   const GROUND_THICKNESS = 0.1;
-  const NET_THICKNESS = 0.05;
+  const NET_THICKNESS = 0.15;
   const PLAYER_STARTING_POS = 10 * 0.7;
   const BALL_STARTING_POS = {
-    x: 0.1 * WIDTH,
-    y: 3 * NET_HEIGHT,
+    x: 0.01 * WIDTH,
+    y: 5,
   };
   const GRAVITY = 10;
   const PHYSICS = {
@@ -39,39 +41,61 @@
 
   const gravity = new b2Vec2(0, -GRAVITY);
   const world = new b2World(gravity);
-  const groundBodyDef = new b2BodyDef();
-  groundBodyDef.position.Set(0, -GROUND_THICKNESS / 2);
-  const groundBody = world.CreateBody(groundBodyDef);
-  // Define the ground box shape.
-  const groundBox = new b2PolygonShape();
-  // The extents are the half-widths of the box.
-  groundBox.SetAsBox(WIDTH, GROUND_THICKNESS / 2);
-  // Add the ground fixture to the ground body.
-  groundBody.CreateFixture(groundBox, 0);
+  {
+    const groundDef = new b2BodyDef();
+    groundDef.position.Set(0, -GROUND_THICKNESS / 2);
+    groundDef.userData = { kind: "GROUND" };
+    const ground = world.CreateBody(groundDef);
+    // Define the ground box shape.
+    const groundBox = new b2PolygonShape();
+    // The extents are the half-widths of the box.
+    groundBox.SetAsBox(WIDTH, GROUND_THICKNESS / 2);
+    // Add the ground fixture to the ground body.
+    ground.CreateFixture(groundBox, 0);
+  }
 
-  const ballDef = new b2BodyDef();
-  ballDef.type = b2BodyType.b2_dynamicBody;
-  ballDef.position.Set(BALL_STARTING_POS.x, BALL_STARTING_POS.y);
-  const ballBody = world.CreateBody(ballDef);
-  const ballShape = new b2CircleShape();
-  ballShape.Set({ x: 0, y: 0 }, BALL_RADIUS);
-  const ballFixtureDef = new b2FixtureDef();
-  ballFixtureDef.shape = ballShape;
-  ballFixtureDef.density = 1;
-  ballFixtureDef.restitution = BALL_RESTITUTION;
-  ballFixtureDef.friction = BALL_RESTITUTION;
-  ballBody.CreateFixture(ballFixtureDef);
-  let ballPosition = ballBody.GetPosition();
+  let ballBody: b2Body;
+  let ballPosition: XY;
+  {
+    const ballDef = new b2BodyDef();
+    ballDef.type = b2BodyType.b2_dynamicBody;
+    ballDef.position.Set(BALL_STARTING_POS.x, BALL_STARTING_POS.y);
+    ballDef.userData = { kind: "BALL" };
+    ballBody = world.CreateBody(ballDef);
+    const ballShape = new b2CircleShape();
+    ballShape.Set({ x: 0, y: 0 }, BALL_RADIUS);
+    const ballFixtureDef = new b2FixtureDef();
+    ballFixtureDef.shape = ballShape;
+    ballFixtureDef.density = 1;
+    ballFixtureDef.restitution = BALL_RESTITUTION;
+    ballFixtureDef.friction = BALL_RESTITUTION;
+    ballBody.CreateFixture(ballFixtureDef);
+    ballPosition = ballBody.GetPosition();
+  }
 
-  function makePlayer(startX: number) {
+  {
+    const netDef = new b2BodyDef();
+    netDef.position.Set(0, NET_HEIGHT / 2);
+    const netBody = world.CreateBody(netDef);
+    const netShape = new b2PolygonShape();
+    netShape.SetAsBox(NET_THICKNESS / 2, NET_HEIGHT / 2);
+    const netFixtureDef = new b2FixtureDef();
+    netFixtureDef.shape = netShape;
+    netFixtureDef.restitution = BALL_RESTITUTION;
+    netFixtureDef.friction = BALL_RESTITUTION;
+    netBody.CreateFixture(netFixtureDef);
+  }
+
+  function makePlayer(startX: number, playerIndex: number) {
     const playerDef = new b2BodyDef();
     playerDef.type = b2BodyType.b2_kinematicBody;
     playerDef.position.Set(startX, 0);
+    playerDef.userData = { kind: "PLAYER", playerIndex: playerIndex };
     const playerBody = world.CreateBody(playerDef);
     const playerShape = new b2ChainShape();
     const playerPoints = [];
     for (let p = 0; p < PLAYER_CHAIN_SIZE; p++) {
-      const angle = Math.PI * (1 - (p / (PLAYER_CHAIN_SIZE - 1)));
+      const angle = Math.PI * (1 - p / (PLAYER_CHAIN_SIZE - 1));
       playerPoints.push({
         x: Math.cos(angle) * PLAYER_RADIUS,
         y: Math.sin(angle) * PLAYER_RADIUS,
@@ -86,13 +110,33 @@
     return playerBody;
   }
 
-  let player1Body = makePlayer(BALL_STARTING_POS.x*1.02)
+  let player1Body = makePlayer(WIDTH / 3, 0);
   let player1Position = player1Body.GetPosition();
-  let player2Body = makePlayer(-WIDTH / 2)
+  let player2Body = makePlayer(-WIDTH / 2, 1);
   let player2Position = player2Body.GetPosition();
 
+  interface AnnotatedBody extends Omit<b2Body, "m_userData"> {
+    m_userData: null | {
+      kind?: "BALL" | "GROUND" | "PLAYER";
+      playerIndex?: number;
+    };
+  }
   class MyListener extends b2ContactListener {
-    override BeginContact(contact): void {
+    override BeginContact(contact: b2Contact): void {
+      const bodies: AnnotatedBody[] = [
+        contact.m_fixtureA.m_body,
+        contact.m_fixtureB.m_body,
+      ];
+      const player = bodies.find((b) => b.m_userData?.kind == "PLAYER");
+      const ball = bodies.find((b) => b.m_userData?.kind == "BALL");
+      const ground = bodies.find((b) => b.m_userData?.kind == "GROUND");
+
+      if (player && ball) {
+        console.log("player", player.m_userData!.playerIndex!);
+      }
+      if (ball && ground) {
+        stopAll = true;
+      }
     }
   }
   world.SetContactListener(new MyListener());
@@ -124,6 +168,7 @@
     ball.y += timeAfterBounce * ball.vy;
     return true;
   }*/
+  let stopAll = false;
 
   let lastPhysics = performance.now() / 1000;
   function runPhysics() {
@@ -146,12 +191,14 @@
 
   onMount(async () => {
     let i = 0;
-    let rid = requestAnimationFrame(function update() {
+    let loopId = requestAnimationFrame(function update() {
       i++;
       runPhysics();
-      rid = requestAnimationFrame(update);
+      if (!stopAll) {
+        loopId = requestAnimationFrame(update);
+      }
     });
-    return () => cancelAnimationFrame(rid);
+    return () => cancelAnimationFrame(loopId);
   });
 
   const computePlayerAngle = (player: XY) => {
@@ -193,6 +240,13 @@
       cx={ballPosition.x}
       cy={ballPosition.y}
       r={BALL_RADIUS}
+    />
+    <rect
+      fill="gray"
+      width={NET_THICKNESS}
+      height={NET_HEIGHT}
+      x={-NET_THICKNESS / 2}
+      y="0"
     />
   </g>
 </svg>
